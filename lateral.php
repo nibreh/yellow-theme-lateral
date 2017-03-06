7722486bed3521cc76a4ccffe5e0c494ccd04331
Juh <nibreh@users.noreply.github.com>
2017-03-06 17:04:35 +0100

    Add files via upload

---
+++ lateral.php
@@ -0,0 +1,12 @@
+<?php
+// Lateral theme, https://github.com/datenstrom/yellow-themes/tree/master/flatsite
+// Copyright (c) 2013-2017 Datenstrom, https://datenstrom.se
+// This file may be used and distributed under the terms of the public license.
+
+class YellowThemeLateral
+{
+	const VERSION = "0.6.0";	
+}
+
+$yellow->themes->register("lateral", "YellowThemeLateral", YellowThemeLateral::VERSION);
+?>
